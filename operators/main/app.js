44519ce351a7fb8fdeda7082c67662df72d60a4b
chenchunaidu-hotstar <maddinenichenchunaidu@gmail.com>
2025-02-18 21:17:58 +0530

    feat: add opertator

---
+++ operators/main/app.js
@@ -0,0 +1,15 @@
+var a = 5;
+var b = 10;
+var c = a + b;
+console.log(c);
+
+var firstName = "maddineni";
+var lastName = "nagateja";
+var fullName = firstName + lastName;
+console.log(fullName);
+
+var sub = a - b;
+var m = a * b;
+var d = a / b;
+var rem = a % b;
+console.log(sub, m, d, rem);
