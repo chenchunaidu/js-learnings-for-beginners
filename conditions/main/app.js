453d908a356bf9945c329e0151e6f5706efbc32b
chenchunaidu-hotstar <maddinenichenchunaidu@gmail.com>
2025-02-25 21:35:02 +0530

    feat: added conditions

---
+++ conditions/main/app.js
@@ -0,0 +1,20 @@
+var a = "red";
+var yellow = "yellow";
+
+if (a == yellow) {
+  console.log("mango");
+} else {
+  console.log("Not mango");
+}
+
+if (a == "red") {
+  console.log("apple");
+} else {
+  console.log("Not apple");
+}
+
+if (a == "orange") {
+  console.log("orange");
+} else {
+  console.log("Not orange");
+}
---
+++ conditions/main/readme.md
@@ -0,0 +1,9 @@
+#### Conditions
+
+1. Conditions anevi edina true na false aa ani check cheyyadaniki use chestaru
+
+2. Okavela condition true ayithe if block lo unna code run avutundi
+
+3. Okavela condition false ayithe else block lo unna code run avutundi
+
+4. Else block lekapothe emi code run avadau
