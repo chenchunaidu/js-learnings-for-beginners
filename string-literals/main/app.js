585db1119ab6ae4b1c04064ed081871818851fe4
chenchunaidu-hotstar <maddinenichenchunaidu@gmail.com>
2025-03-17 21:41:45 +0530

    feat: added code

---
+++ string-literals/main/app.js
@@ -0,0 +1,7 @@
+let firstName = "maddineni";
+let lastName = "nani";
+let fullName = firstName + " " + lastName;
+let string = `${firstName} ${lastName}`;
+console.log(fullName);
+console.log(string);
+console.log(`my name is ${firstName} ${lastName}`);
---
+++ string-literals/main/readme.md
@@ -0,0 +1,6 @@
+### string-literals
+
+1. string literals ni string formatting cheyyadaniki upayogistaru
+2. string literals vadadaniki manam ` ni use chestam
+3. string ni normal gane rastam
+4. variables ni `${firstName}` dollar brackets lo pedatham
